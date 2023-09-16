ed23078273bb56086a22274c3a1d2af3490eb5d2
youbo <42180410+spongebody@users.noreply.github.com>
2023-09-16 15:38:56 +0800

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -24,9 +24,8 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+        className="dark:bg-tertiary bg-ltertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
-{/*         背景设为黑色的 */}
         <div className="relative w-full h-[230px]">
           <img
             src={image}
